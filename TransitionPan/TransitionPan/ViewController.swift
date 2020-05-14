bd4abe0dda7c1c6e9b6b8edffe60171b5f9127c5
kifio <kg.of.morphine@gmail.com>
2020-05-14 23:16:04 +0200

    TransitionPan

---
+++ TransitionPan/TransitionPan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionPan
+//
+//  Created by Ivan Murashov on 12.05.20.
+//  Copyright © 2020 Ivan Murashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
